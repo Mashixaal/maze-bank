757315935f6ff40593f5c0f6a915db7eba7e32ab
mashixaal <safwan7342@gmail.com>
2022-09-13 21:20:40 +0200

    first commit

---
+++ Tips to start coding maze-bank/tips.js
@@ -0,0 +1,15 @@
+// visuel
+// Des identifiants
+
+// Formulaires, input, boutton envoyé
+
+// algo
+// récuperer boutton submit
+// récuperer la valeur input
+// créer une variable qui va stocker notre solde
+// créer une fonction quand on click sur le boutton envoyé récup les champs
+// innerhtml
+// boutton ajouter de l'argent, et retirer de l'argent
+// ex : argument input.value
+// a découvert
+// pouvoir envoyé de l'argent à un bénéficiaire
